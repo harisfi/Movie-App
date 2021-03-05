b66e83fd2a5997c68004a2d73c1fdeda63481a5d
harisfi <acc.cp.haris@gmail.com>
2021-03-05 08:50:12 +0700

    Redesign layouts

--- app/src/main/java/com/hryzx/movieapp/ui/detail/DetailCinemaActivity.java
+++ app/src/main/java/com/hryzx/movieapp/ui/detail/DetailCinemaActivity.java
@@ -3,40 +3,53 @@ package com.hryzx.movieapp.ui.detail;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
+import com.google.android.material.appbar.AppBarLayout;
 import com.hryzx.movieapp.R;
 import com.hryzx.movieapp.data.CastEntity;
 import com.hryzx.movieapp.data.CinemaEntity;
 import com.hryzx.movieapp.databinding.ActivityDetailCinemaBinding;
-import com.hryzx.movieapp.databinding.ContentDetailCinemaBinding;
 import com.hryzx.movieapp.utils.DataDummy;
 
 import java.util.List;
 
 public class DetailCinemaActivity extends AppCompatActivity {
     public static final String EXTRA_MOVIE = "extra_movie";
-    private ContentDetailCinemaBinding detailContentBinding;
+    private ActivityDetailCinemaBinding activityDetailCinemaBinding;
     private String url;
+    private Menu collapseMenu;
+    private boolean appBarExpanded = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityDetailCinemaBinding activityDetailCinemaBinding = ActivityDetailCinemaBinding.inflate(getLayoutInflater());
-        detailContentBinding = activityDetailCinemaBinding.detailContent;
-
+        activityDetailCinemaBinding = ActivityDetailCinemaBinding.inflate(getLayoutInflater());
         setContentView(activityDetailCinemaBinding.getRoot());
 
-        setSupportActionBar(activityDetailCinemaBinding.toolbar);
-
+        setSupportActionBar(activityDetailCinemaBinding.animToolbar);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+        activityDetailCinemaBinding.collapsingToolbar.setTitle(getString(R.string.title_activity_detail_movie));
+
+        activityDetailCinemaBinding.appbar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if(Math.abs(verticalOffset) > 200){
+                    appBarExpanded = false;
+                }else{
+                    appBarExpanded = true;
+                }
+                invalidateOptionsMenu();
+            }
+        });
 
         DetailCinemaAdapter adapter = new DetailCinemaAdapter();
         DetailCinemaViewModel viewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(DetailCinemaViewModel.class);
@@ -56,39 +69,66 @@ public class DetailCinemaActivity extends AppCompatActivity {
                 adapter.setCasts(casts);
             }
         }
-        detailContentBinding.rvCast.setAdapter(adapter);
+        activityDetailCinemaBinding.rvCast.setAdapter(adapter);
 
-        detailContentBinding.btnWatch.setOnClickListener(v -> {
+        activityDetailCinemaBinding.btnWatch.setOnClickListener(v -> {
             Uri uri = Uri.parse(url);
             Intent intent = new Intent(Intent.ACTION_VIEW, uri);
             startActivity(intent);
         });
-
-        detailContentBinding.btnShare.setOnClickListener(v -> {
-            Intent intent = new Intent();
-            intent.setAction(Intent.ACTION_SEND);
-            intent.setType("text/plain");
-            intent.putExtra(Intent.EXTRA_TEXT, url);
-            startActivity(Intent.createChooser(intent, "Share"));
-        });
+        activityDetailCinemaBinding.fabShare.setOnClickListener(v -> showShare());
     }
 
     private void populateCinema(CinemaEntity cinemaEntity) {
         float rating = Float.parseFloat(String.valueOf(cinemaEntity.getRating()));
         rating = (rating / 10) * 5;
-        detailContentBinding.textTitle.setText(cinemaEntity.getTitle());
-        detailContentBinding.rating.setRating(rating);
-        detailContentBinding.textRating.setText(String.valueOf(cinemaEntity.getRating()));
-        detailContentBinding.textDate.setText(cinemaEntity.getReleaseDate());
-        detailContentBinding.textGenre.setText(cinemaEntity.getGenre());
-        detailContentBinding.textDuration.setText(cinemaEntity.getDuration());
-        detailContentBinding.textOverview.setText(cinemaEntity.getOverview());
+        activityDetailCinemaBinding.collapsingToolbar.setTitle(cinemaEntity.getTitle());
+        activityDetailCinemaBinding.textTitle.setText(cinemaEntity.getTitle());
+        activityDetailCinemaBinding.rating.setRating(rating);
+        activityDetailCinemaBinding.textRating.setText(String.valueOf(cinemaEntity.getRating()));
+        activityDetailCinemaBinding.textDate.setText(cinemaEntity.getReleaseDate());
+        activityDetailCinemaBinding.textGenre.setText(cinemaEntity.getGenre());
+        activityDetailCinemaBinding.textDuration.setText(cinemaEntity.getDuration());
+        activityDetailCinemaBinding.textOverview.setText(cinemaEntity.getOverview());
 
         Glide.with(this)
                 .load(cinemaEntity.getImagePath())
-                .transform(new RoundedCorners(20))
                 .apply(RequestOptions.placeholderOf(R.drawable.ic_loading)
                         .error(R.drawable.ic_error))
-                .into(detailContentBinding.imagePoster);
+                .into(activityDetailCinemaBinding.imagePoster);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        if(collapseMenu != null && (!appBarExpanded || collapseMenu.size() != 1)){
+            //collapsed
+            collapseMenu.add("Share")
+                    .setIcon(R.drawable.ic_share)
+                    .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        }
+        return super.onPrepareOptionsMenu(collapseMenu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        this.collapseMenu = menu;
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getTitle() == "Share"){
+            showShare();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    void showShare() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, url);
+        startActivity(Intent.createChooser(intent, "Share"));
     }
 }
